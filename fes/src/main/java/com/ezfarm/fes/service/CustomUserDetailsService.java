12f5881e854145f7cf1b1339589df494ed051a14
hyred <hyukhee114@gmail.com>
2022-04-29 17:07:39 +0900

    차트 구현 하는 중

--- fes/src/main/java/com/ezfarm/fes/service/CustomUserDetailsService.java
+++
@@ -1,7 +0,0 @@
-package com.ezfarm.fes.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface CustomUserDetailsService extends UserDetailsService {
-
-}
