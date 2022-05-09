94e6029b9e72d93a6e393549b4d8efb28acf793b
parkjonghhyeok <104114403+parkjonghhyeok@users.noreply.github.com>
2022-05-09 11:10:54 +0900

    2022-05-09

--- fes/src/main/resources/static/js/self-custom-monthgraphcheck.js
+++ fes/src/main/resources/static/js/self-custom-monthgraphcheck.js
@@ -1,17 +1,18 @@
-	$('#chartSearch').on(
-			"click",
-			function() {
-				var gc= $('#GraphChoice').val();		
-				var startDate = $('#FirstDate').val();
-				var endDate = $('#SecondDate').val();
-				if (startDate === "") {
-					alert("시작 날짜를 선택해주세요.");
-					return false;
-				} else if (endDate === "") {
-					alert("종료 날짜를 선택해주세요.");
-					return false;
-				}
-				location.href = '/fes/search?gcCondition=' + gc
-						+ '&startCondition=' + startDate + '&endCondition='
-						+ endDate;
-			});
+	$('#chartSearchM').on("click",function(){
+    var gc = $(this).prev().prev().prev().prev().val();
+    var startDate = $(this).prev().prev().prev().val();
+    var endDate = $(this).prev().val();
+    chartSearch(gc, startDate, endDate);
+});
+function chartSearch(gc, startDate, endDate){
+    if (startDate === "") {
+        alert("시작 날짜를 선택해주세요.");
+        return false;
+    } else if (endDate === "") {
+        alert("종료 날짜를 선택해주세요.");
+        return false;
+    }
+    location.href = '/fes/search?gcCondition=' + gc
+            + '&startCondition=' + startDate + '&endCondition='
+            + endDate;
+}
