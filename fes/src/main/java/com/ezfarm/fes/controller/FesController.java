8a7fdccb91fbca346428f10b2887e64206c6e3f4
hyred <hyukhee114@gmail.com>
2022-05-09 15:03:24 +0900

    총 수를 정수로 표기하도록, 디폴트 기간도 표기, 진짜진짜 끝

--- fes/src/main/java/com/ezfarm/fes/controller/FesController.java
+++ fes/src/main/java/com/ezfarm/fes/controller/FesController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.ezfarm.fes.elastic.service.ElasticService;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/fes")
@@ -21,6 +22,19 @@ public class FesController {
 	private ElasticService service;
 	public static String keepSearch[][] = {{"default", "default", "default"},{"default", "default", "default"},{"default", "default", "default"}};
 	Logger logger = LoggerFactory.getLogger(FesController.class);
+
+	private LocalDate now = LocalDate.now();
+	private LocalDate pastOfDays = now.minusDays(7); // 디폴트 7일
+	private LocalDate pastOfWeeks = now.minusWeeks(5); // 디폴트 5주
+	private LocalDate pastOfMonths = now.minusMonths(6); // 디폴트 6개월
+	private final LocalDate PAST_LIMITED = LocalDate.of(2022,03,01);
+
+
+
+
+
+
+
 
 	@GetMapping("/search")
 	public String search(Model model,
@@ -50,10 +64,11 @@ public class FesController {
 		model.addAttribute("all", service.fesSearch());
 
 		if(keepSearch[0][0].equals("default")){
+			pastOfDays = (pastOfDays.isBefore(PAST_LIMITED)) ? PAST_LIMITED : pastOfDays;
 			model.addAttribute("day", service.fesDailySearch(condition));
 			model.addAttribute("conditionD", "default");
-			model.addAttribute("firstDateD", "default");
-			model.addAttribute("endDateD", "default");
+			model.addAttribute("firstDateD", pastOfDays);
+			model.addAttribute("endDateD", now);
 		}else{
 			condition[0] = keepSearch[0][0];
 			condition[1] = keepSearch[0][1];
@@ -65,10 +80,11 @@ public class FesController {
 		}
 
 		if(keepSearch[1][0].equals("default")){
+			pastOfWeeks = (pastOfWeeks.isBefore(PAST_LIMITED)) ? PAST_LIMITED : pastOfWeeks;
 			model.addAttribute("week", service.fesWeekSearch(condition));
 			model.addAttribute("conditionW", "default");
-			model.addAttribute("firstDateW", "default");
-			model.addAttribute("endDateW", "default");
+			model.addAttribute("firstDateW", pastOfWeeks);
+			model.addAttribute("endDateW", now);
 		}else {
 			condition[0] = keepSearch[1][0];
 			condition[1] = keepSearch[1][1];
@@ -80,10 +96,11 @@ public class FesController {
 		}
 
 		if(keepSearch[2][0].equals("default")){
+			pastOfMonths = (pastOfMonths.isBefore(PAST_LIMITED)) ? PAST_LIMITED : pastOfMonths;
 			model.addAttribute("month", service.fesMonthSearch(condition));
 			model.addAttribute("conditionM", "default");
-			model.addAttribute("firstDateM", "default");
-			model.addAttribute("endDateM", "default");
+			model.addAttribute("firstDateM", pastOfMonths);
+			model.addAttribute("endDateM", now);
 		}else {
 			condition[0] = keepSearch[2][0];
 			condition[1] = keepSearch[2][1];
