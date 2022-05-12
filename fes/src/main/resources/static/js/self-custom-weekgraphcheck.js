24e8616bb99657d6429445497b8f877a663872ae
parkjonghhyeok <104114403+parkjonghhyeok@users.noreply.github.com>
2022-05-12 11:57:21 +0900

    최종본

--- fes/src/main/resources/static/js/self-custom-weekgraphcheck.js
+++ fes/src/main/resources/static/js/self-custom-weekgraphcheck.js
@@ -1,7 +1,30 @@
 $('#chartSearchW').on("click",function() {
     var gc = $(this).prev().prev().prev().prev().val();
-    var startDate = $(this).prev().prev().prev().val();
-    var endDate = $(this).prev().val();
+    var startDate = $(this).prev().prev().prev().val();;
+    var endDate = $(this).prev().val();;
     chartSearch(gc, startDate, endDate);
 });
 
+//날짜조정
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10){
+  dd='0'+dd
+} 
+if(mm<10){
+  mm='0'+mm
+} 
+today = yyyy+'-'+mm+'-'+dd;
+
+/*$(".week-firstdate").attr("min", "2022-03-25");
+$(".week-firstdate").attr("max", today);
+$(".week-seconddate").attr("min", "2022-03-25");
+$(".week-seconddate").attr("max", today);*/
+
+
+document.getElementsByClassName("firstdate")[1].setAttribute("min","2022-03-25");
+document.getElementsByClassName("firstdate")[1].setAttribute("max", today);
+document.getElementsByClassName("seconddate")[1].setAttribute("min", "2022-03-25");
+document.getElementsByClassName("seconddate")[1].setAttribute("max", today);
